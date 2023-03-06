c22d20a028da426d4745461aaac173f269db0dc0
Ericcost <eric.costerousse@gmail.com>
2023-03-06 13:29:11 +0100

    Logn Modification

--- immocoin_app/src/pages/Login/Login.jsx
+++ immocoin_app/src/pages/Login/Login.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import './Login.scss'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className='login'>
+      <p>
+        Login
+      </p>
+    </div>
   )
 }
