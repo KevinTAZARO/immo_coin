1d72dba1b173278618b3efbd3f3928d903e5bb0c
Ericcost <eric.costerousse@gmail.com>
2023-03-06 13:25:31 +0100

    Navbar Test

--- immocoin_app/src/components/Navbar/Navbar.jsx
+++ immocoin_app/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Navbar.scss'
 
 export default function Navbar() {
   return (
     <div className='navbar'>
-      <p>Navbar</p>
+      <div className='logo'>
+        <Link to="/"> 
+          ImmoCoin
+        </Link>
+      </div>
+      <div className='authentication'>
+        <Link to="/login">
+        se connecter
+        </Link>
+        <Link to="/register">
+          s'enregister
+        </Link>
+      </div>
     </div>
   )
 }
