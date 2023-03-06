440b2470cdaea42947d1e39d17ce8a47adde4410
Ericcost <eric.costerousse@gmail.com>
2023-03-06 13:29:24 +0100

    Register Modification

--- immocoin_app/src/pages/Register/Register.jsx
+++ immocoin_app/src/pages/Register/Register.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import './Register.scss'
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div className='register'>
+      <p>
+        Register
+      </p>
+    </div>
   )
 }
