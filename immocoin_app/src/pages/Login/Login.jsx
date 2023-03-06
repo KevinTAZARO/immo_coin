e64a62a9578f004526408287b8729a8955d65b60
BenjaminCharmes <benjamin.charmes@gmail.com>
2023-03-06 19:14:05 +0000

    Add login page, backend link, frontend todo

--- immocoin_app/src/pages/Login/Login.jsx
+++ immocoin_app/src/pages/Login/Login.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react'
 import { authAPI } from '../../services/fetchData'
+import { useAtom } from 'jotai';
+import userAtom from '../../stores/userStore';
+import './Login.scss'
 
 const Login = () => {
+
+  const [user, setUser] = useAtom(userAtom);
 
   const [formData, setFormData] = useState(
     {
@@ -17,16 +22,18 @@ const Login = () => {
     });
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const formDataToSent = { "user": formData }
-    console.log(formDataToSent)
-    authAPI.login(formData)
-
+    try {
+      const response = await authAPI.login(formDataToSent, setUser);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
-    <div>
+    <div className='login'>
       <form action="" onSubmit={handleSubmit}>
         <input type="email" name='email' placeholder='Email' value={formData.email} onChange={handleChange}/>
         <input type="password" name='password' placeholder='Mot de passe' value={formData.password} onChange={handleChange}/>
@@ -37,4 +44,4 @@ const Login = () => {
 
 }
 
-export default Login
+export default Login;
