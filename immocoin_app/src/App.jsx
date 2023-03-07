911e24dccf804e0dfba1467c0b6836fc7b3d0297
Ericcost <eric.costerousse@gmail.com>
2023-03-07 11:15:33 +0100

    Protected Routes ADD

--- immocoin_app/src/App.jsx
+++ immocoin_app/src/App.jsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Cookies from 'js-cookie'
-import { authAPI } from './services/fetchData';
+import { BrowserRouter as Router, Routes, Route, Outlet, Navigate } from "react-router-dom";
 import { useAtom } from 'jotai';
+import Cookies from 'js-cookie'
+
+import { authAPI } from './services/fetchData';
 import userAtom from './stores/userStore';
 import './styles/style.scss';
 
@@ -9,7 +10,6 @@ import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Profile from "./pages/Profile/Profile";
-import './styles/style.scss'
 
 function App() {
 
@@ -20,11 +20,30 @@ function App() {
   if (cookieExist && user.logged === false) {
     let data = " Bearer " + auth_token
     authAPI.loginWithToken(data, setUser);
+    
   }
+
+  function Layout() {
+    return (
+      <div>
+        <Outlet />
+      </div>
+    );
+  }
+
+  function ProtectedRoute({ children }) {
+    if (user.logged === false) {
+      return <Navigate to="/sign_in" />;
+    };
+
+    return children;
+  } 
+
+  
   
   return (
     <>
-      <BrowserRouter>
+      <Router>
         <div className="App">
           <main>
             <Routes>
@@ -32,11 +51,24 @@ function App() {
               <Route path="/sign_in" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/sign_out" />
-              <Route path="/profile/:id" element={<Profile />} />
+              <Route 
+                path="/profile/:id" 
+                element={
+                  <Layout>
+                    <ProtectedRoute>
+                      <Profile />
+                    </ProtectedRoute>
+                  </Layout>
+                }
+                children= {[
+                  <Route key="profile" path="/profile/:id" element={<Profile />} />
+                ]} 
+              />
+              
             </Routes>
           </main>
         </div>
-      </BrowserRouter>
+      </Router>
     </>
   )
 }
