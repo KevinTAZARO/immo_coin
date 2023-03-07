b141ff99ca4ef3330024171ed748faf22c8131de
Ericcost <eric.costerousse@gmail.com>
2023-03-07 17:46:10 +0100

    New Register Form

--- immocoin_app/src/pages/Register/Register.jsx
+++ immocoin_app/src/pages/Register/Register.jsx
@@ -48,39 +48,39 @@ const Register = () => {
     <div className='register'>
       <Navbar />
       <section className="signup">
-            <div className="container">
-                <div className="signup-content">
-                    <div className="signup-form">
-                        <h2 className="form-title">Inscription</h2>
-                        <form action="" className="register-form" id="register-form" onSubmit={handleSubmit}>
-                            <div className="form-group">
-                                <label for="name"><i className="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="username" id="username" placeholder="Nom de compte" onChange={handleChange}/>
-                            </div>
-                            <div className="form-group">
-                                <label><i className="zmdi zmdi-email"></i></label>
-                                <input type="email" name="email" id="email" placeholder="Email" onChange={handleChange}/>
-                            </div>
-                            <div className="form-group">
-                                <label><i className="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="password" id="password" placeholder="Mot de passe" onChange={handleChange}/>
-                            </div>
-                            <div className="form-group">
-                                <label><i className="zmdi zmdi-lock-outline"></i></label>
-                                <input type="password" name="passwordConfirmation" id="passwordConfirmation" placeholder="Confirmer le mot de passe" onChange={handleChange}/>
-                            </div>
-                            <div className="form-group form-button">
-                                <input type="submit" name="signup" id="signup" className="form-submit" value="S'inscrire"/>
-                            </div>
-                        </form>
-                    </div>
-                    <div className="signup-image">
-                        <figure><img src="./src/pages/Register/signup-image.jpg" alt="sing up image"/></figure>
-                        <Link to="/sign_in" className="signup-image-link">Je suis déjà inscrit.</Link>
-                    </div>
-                </div>
+        <div className="container">
+          <div className="signup-content">
+              <div className="signup-form">
+                <h2 className="form-title">Inscription</h2>
+                <form action="" className="register-form" id="register-form" onSubmit={handleSubmit}>
+                  <div className="form-group">
+                    <label for="name"><i className="zmdi zmdi-account material-icons-name"></i></label>
+                    <input type="text" name="username" id="username" placeholder="Nom de compte" onChange={handleChange}/>
+                  </div>
+                  <div className="form-group">
+                    <label><i className="zmdi zmdi-email"></i></label>
+                    <input type="email" name="email" id="email" placeholder="Email" onChange={handleChange}/>
+                  </div>
+                  <div className="form-group">
+                    <label><i className="zmdi zmdi-lock"></i></label>
+                    <input type="password" name="password" id="password" placeholder="Mot de passe" onChange={handleChange}/>
+                  </div>
+                  <div className="form-group">
+                    <label><i className="zmdi zmdi-lock-outline"></i></label>
+                    <input type="password" name="passwordConfirmation" id="passwordConfirmation" placeholder="Confirmer le mot de passe" onChange={handleChange}/>
+                  </div>
+                  <div className="form-group form-button">
+                    <input type="submit" name="signup" id="signup" className="form-submit" value="S'inscrire"/>
+                  </div>
+                </form>
+              </div>
+            <div className="signup-image">
+              <figure><img src="src/assets/images/signup-image.jpg" alt="sing up image"/></figure>
+              <Link to="/sign_in" className="signup-image-link">Je suis déjà inscrit.</Link>
             </div>
-        </section>
+          </div>
+        </div>
+      </section>
       <Footer />
     </div>
   )
