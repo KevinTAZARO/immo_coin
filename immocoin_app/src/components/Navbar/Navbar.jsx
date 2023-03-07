0706cf69c9bcf77b26327094bf08958438d8e342
KevinTAZARO <kenee23@hotmail.fr>
2023-03-07 15:43:17 +0100

    redo navbar

--- immocoin_app/src/components/Navbar/Navbar.jsx
+++ immocoin_app/src/components/Navbar/Navbar.jsx
@@ -4,41 +4,38 @@ import userAtom from "../../stores/userStore";
 import { useAtomValue } from "jotai";
 import { authAPI } from "../../services/fetchData";
 import { useAtom } from "jotai";
-import Cookies from "js-cookie"
+import Cookies from "js-cookie";
+import { GoSignIn } from "react-icons/go";
+import { BsPersonFillCheck } from "react-icons/bs";
 
 const Navbar = () => {
-
   // const auth_token = Cookies.get('auth_token');
 
-  const userInfo = useAtomValue(userAtom)
+  const userInfo = useAtomValue(userAtom);
 
   // const [user, setUser] = useAtom(userAtom);
   // const header = 'Bearer ' + auth_token
 
   return (
-    <div className='navbar'>
-      <div className='logo'>
-        <Link to="/"> 
-          ImmoCoin
-        </Link>
-        <p>{userInfo.user.username}</p>
-        <p>{userInfo.user.email}</p>
+    <div className="navbar">
+      <div className="logo">
+          <Link to="/">
+            <strong>Immo</strong>Coin
+          </Link>
       </div>
-      <div className='authentication'>
-        <Link to="/sign_in">
-          Connexion
-        </Link>
-        <Link to="/register">
-          S'inscrire
-        </Link>
+      <div className="authentication">
+        <div className="signin">
+          <BsPersonFillCheck />
+          <Link to="/sign_in">Connexion</Link>
+        </div>
+        <div className="reg">
+          <GoSignIn />
+          <Link to="/register">S'inscrire</Link>
+        </div>
         {/* <button onClick={authAPI.logout(header, setUser)}>Se déconnecter</button> */}
-        
-    
-        
       </div>
     </div>
-  )
-
-}
+  );
+};
 
 export default Navbar;
